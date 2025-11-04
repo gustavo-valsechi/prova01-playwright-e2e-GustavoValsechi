19464241d92976df3a1e4effe6eaac2beac2a861
Gustavo Valsechi De Freitas <gustavo.24110@alunosatc.edu.br>
2025-11-04 20:43:32 -0300

    last commit

--- src/scenarios/ZeroStepExample.spec.ts
+++ src/scenarios/ZeroStepExample.spec.ts
@@ -4,11 +4,11 @@ import { join } from 'path';
 import { TheConfig } from 'sicolo';
 
 test('zerostep example', async ({ page }) => {
-  // const CONFIG = join(__dirname, '../support/fixtures/config.yml');
-  // const BASE_URL = TheConfig.fromFile(CONFIG).andPath('application.base_url').retrieveData();
+  const CONFIG = join(__dirname, '../support/fixtures/config.yml');
+  const BASE_URL = TheConfig.fromFile(CONFIG).andPath('application.base_url').retrieveData();
 
-  // await page.goto(BASE_URL + '/category');
+  await page.goto(BASE_URL + '/category');
 
-  // const aiArgs = { page, test };
-  // await ai('click on button with plus icon', aiArgs);
+  const aiArgs = { page, test };
+  await ai('click on button with plus icon', aiArgs);
 });
